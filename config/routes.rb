87b8b6fee6eec6ee6e96c1fe15eadada59fc6f52
Grace Mallett <gmmallet@bowdoin.edu>
2017-05-25 02:30:42 +0000

    comment stuff

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Nomster::Application.routes.draw do
 
   devise_for :users
-  
+
   root 'places#index'
 
-  resources :places
+  resources :places do
+    resources :comments, only: :create
+  end
+
 
 
   # The priority is based upon order of creation: first created -> highest priority.
